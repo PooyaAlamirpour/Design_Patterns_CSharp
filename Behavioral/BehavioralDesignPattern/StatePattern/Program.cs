c221654d626371c608675af85a7e3a38fc00f946
Pooya Alamirpour <pooya.alamirpour@gmail.com>
2021-09-26 20:50:08 +0330

    StatePattern is added.

---
+++ Behavioral/BehavioralDesignPattern/StatePattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using StatePattern.Contexts;
+
+namespace StatePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new Account("Jim Josh");
+
+            account.Deposit(500.0);
+            account.Deposit(100.0);
+            account.Deposit(550.0);
+            account.PayInterest();
+            account.Withdraw(2000.00);
+            account.Withdraw(1100.00);
+        }
+    }
+}
---
+++ Behavioral/BehavioralDesignPattern/StatePattern/States/State.cs
@@ -0,0 +1,28 @@
+﻿using StatePattern.Contexts;
+
+namespace StatePattern.States
+{
+    public abstract class State
+    {
+        public abstract void Deposit(double amount);
+        public abstract void Withdraw(double amount);
+        public abstract void PayInterest();
+        
+        protected Account account;
+        protected double balance;
+        protected double interest;
+        protected double lowerLimit;
+        protected double upperLimit;
+
+        public Account Account
+        {
+            get => account;
+            set => account = value;
+        }
+        public double Balance
+        {
+            get => balance;
+            set => balance = value;
+        }
+    }
+}
